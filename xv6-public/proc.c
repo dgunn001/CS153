22e36910217fd82bc8683de7c128602a8160f5eb
Darrien Gunn <38056323+dgunn001@users.noreply.github.com>
2020-01-31 13:47:50 -0800

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -397,37 +397,38 @@ waitpid(int pid,int *status, int options){
   int havePid = 0;
 
   acquire(&ptable.lock);
-  for (p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->pid != pid){
-      continue;
-    } else {
-      for(;;) {
-        havePid = 1;
-        if(p->state == ZOMBIE) {
-        //found
-          if (status != 0){
-           *status = p->status;
-          }
-          kfree(p->kstack);
-          p->kstack = 0;
-          freevm(p->pgdir);
-          p->pid = 0;
-          p->parent = 0;
-          p->name[0] = 0;
-          p->killed = 0;
-          p->state = UNUSED;
-          release(&ptable.lock);
-          return pid;
-        }
+  for(;;){
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->pid != pid)
+        continue;
+      havePids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.
+	if(status != 0){
+	 *status = p->status;
+	}
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
       }
     }
-    sleep(curproc, &ptable.lock);
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
-  if(!havePid || curproc->killed) {
-    release(&ptable.lock);
-    return -1;
-  }
-  return -1;
 }
 //PAGEBREAK: 42
 // Per-CPU process scheduler.
