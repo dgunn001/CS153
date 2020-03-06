9b787aaad07237bf9daa5ccea7e1bbe5d4f34cf4
Darrien Gunn <dgunn001@sledge.cs.ucr.edu>
2020-03-05 22:13:53 -0800

    user Space and stack pointer

--- vm.c
+++ vm.c
@@ -337,6 +337,22 @@ copyuvm(pde_t *pgdir, uint sz)
       goto bad;
     }
   }
+  if(pointystack == 0)
+	return d;
+
+  for(i = pointystack; i < USERSPACE; i += PGSIZE){
+      if((pte = walkpgdir(pgdir,(void*)i, 1)) == )
+	panic("copyuvm: pte should exist");
+	if(!(*pte & PTE_P))
+	panic("copyuvm: page not present");
+      pa = PTE_ADDR(*pte);
+      flags = PTE_FLAGS(*pte);
+      if((mem = kalloc()) == 0)
+	goto bad;
+      memmove(mem, (char*)P2V(pa), PGSIZE);
+      if(mappages(d, (void*)i,PGSIZE, V2P(mem),flags) < 0)
+	goto bad;
+   }
   return d;
 
 bad:
